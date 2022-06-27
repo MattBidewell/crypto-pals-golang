a6ce2adcd2de5ceece33c2700a9d7b4443198f4d
Matthew Bidewell <matthewbidewell@gmail.com>
2022-06-27 09:17:13 +0100

    set1: [ challenge3 ]

--- sets/set1/challenge3/challenge.go
+++ sets/set1/challenge3/challenge.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"strings"
 )
 
+// decode a hexadecimal string to a byte array
 func decodeHexString(str string) ([]byte) {
 	hex, err := hex.DecodeString(str)
 	if err != nil {
@@ -14,28 +16,66 @@ func decodeHexString(str string) ([]byte) {
 	return hex
 }
 
-func xorHex(hex1 []byte, test byte)  ([]byte) {
+// xored against a single character (rune)
+func xorHex(hex1 []byte, test rune)  ([]byte) {
 	byteAr3 := make([]byte, len(hex1))
 
 	// xor against test byte
+	// xor example
+	// 110101
+	// 101010
+	// ------ xored
+	// 011111
 	for i := range hex1 {
-		byteAr3[i] = hex1[i] ^ test
+		byteAr3[i] = hex1[i] ^byte(test)
 	}
 
 	return byteAr3
 }
 
+func calculateScore(str string) (int) {
 
-// func testString()
+	testString := "etaoin shrdlu"
+
+	score := 0
+	for _, char := range str {
+		if strings.ContainsRune(testString, char) {
+			score++
+		}
+	}
+	return score
+}
+
+type testResult struct {
+	xorValue string
+	score int
+	xorRune rune
+}
 
 func main() {
-	// testString := "etaoin shrdlu"
 
 	hex := decodeHexString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
+	results := make([]testResult, 256)
+
 	for i := 1; i < 256; i++ {
-		xorHex := xorHex(hex, byte(i))
-		fmt.Println(string(xorHex))
+		xorHex := string(xorHex(hex, rune(i)))
+
+		score := calculateScore(xorHex)
+
+		result := testResult{}
+		result.xorValue = xorHex
+		result.score = score
+		result.xorRune = rune(i)
+
+		results = append(results, result)
 	}
 
+	highest := testResult{}
+	for _, result := range results {
+		if highest.score < result.score {
+			highest = result
+		}
+	}
+	fmt.Printf("String: %s \nRune: %s\nScore: %d", highest.xorValue, string(highest.xorRune), highest.score)
 }
